c6d3c62792660e8f4593840a908ce2dc33016025
SergioGenesis97 <sergio.bautista@alumnos.udg.mx>
2021-03-24 16:47:40 -0600

    Primer Version, Primer Commit

---
+++ app/src/main/java/mx/udg/alumnos/srapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.udg.alumnos.srapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
